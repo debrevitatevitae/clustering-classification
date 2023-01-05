c66e9aace641b0ddfbb952bbf36cd917f77aadfa
debrevitatevitae <giotb92@gmail.com>
2023-01-05 23:55:36 +0100

    pca-iris.py: add

--- pca-iris.py
+++ pca-iris.py
@@ -6,13 +6,7 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-
-
-def my_sum(a:float, b:float) -> float:
-	return a + b
 
 
 if __name__ == '__main__':
 	np.random.seed(0)
-	print(my_sum(3, 5))
