e97c69efd7346f94996f2b2bedc6c9da517e881e
debrevitatevitae <giotb92@gmail.com>
2023-01-19 19:30:33 +0100

    svm-primal-iris.py: plot hyperplane in principal components

--- svm-primal-iris.py
+++ svm-primal-iris.py
@@ -15,13 +15,10 @@ import sklearn.svm as svm
 from utils import compute_pca
 
 
-def plot_iris_2_class_2_pcs(axis, X, y, title='Iris dataset: first two classes', labels=['setosa', 'versicolor']):
-    U, s, VT = compute_pca(X)
-    # print(U.shape)
-    # print(len(s))
-    # print(VT.shape)
-    V = VT.T
-    X_pc2 = V[:, :2]
+def plot_iris_2_class_2_pcs(axis, X, y, U, title='Iris dataset: first two classes', labels=['setosa', 'versicolor']):
+    X_pc2 = np.transpose(U.T @ X.T)
+    # truncate to 2 components
+    X_pc2 = X_pc2[:, :2]
     X_pc2_setosa = X_pc2[y==0]
     X_pc2_versicolor = X_pc2[y==1]
     axis.scatter(X_pc2_setosa[:,0], X_pc2_setosa[:,1], label=labels[0])
@@ -31,7 +28,6 @@ def plot_iris_2_class_2_pcs(axis, X, y, title='Iris dataset: first two classes',
     axis.set_title(title)
     axis.legend()
     return
-
 
 def hyperplane_2pcs(w:np.ndarray, b:float, U:np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """Compute the hyperplanes coordinates into the two principal components of the training data.
@@ -50,9 +46,17 @@ def hyperplane_2pcs(w:np.ndarray, b:float, U:np.ndarray) -> Tuple[np.ndarray, np
     # Truncate to 2 principal components
     w_pcs = w_pcs[:2]
     # Compute the pc-coordinates of the points on the hyperplane
-    X_h_pc1 = np.linspace(-0.5, 0.5, 100)
+    X_h_pc1 = np.linspace(-2., 2., 100)
     X_h_pc2 = - w_pcs[0]/w_pcs[1] * X_h_pc1 - b
     return X_h_pc1, X_h_pc2
+
+def plot_hyperplane_2pcs(axis, w, b, U):
+    X_h_pc1, X_h_pc2 = hyperplane_2pcs(w, b,U)
+    axis.plot(X_h_pc1, X_h_pc2, 'k-', label='hyperplane')
+    axis.set_xlabel('PC 1')
+    axis.set_ylabel('PC 2')
+    axis.legend()
+    return
 	
 
 if __name__ == '__main__':
@@ -71,23 +75,29 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+    U, s, VT = compute_pca(X_train)
     fig, axs = plt.subplots(1, 2)
-    plot_iris_2_class_2_pcs(axs[0], X_train, y_train, title="Train points of Iris")
-    plot_iris_2_class_2_pcs(axs[1], X_test, y_test, title="Test points of Iris")
+    plot_iris_2_class_2_pcs(axs[0], X_train, y_train, U, title="Train points of Iris")
+    plot_iris_2_class_2_pcs(axs[1], X_test, y_test, U, title="Test points of Iris")
     fig.tight_layout()
-    plt.show()
+    # plt.show()
 
     #%% Scale the data
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
     #%% Fit the svm
     classifier = svm.LinearSVC()
     classifier.fit(X_train_scaled, y_train)
     w_opt = classifier.coef_
     b_opt = classifier.intercept_
-    print(w_opt)
-    print(b_opt)
 
     #%% Plot the separating hyperplane
-    
+    fig, axs = plt.subplots(1, 2)
+    plot_iris_2_class_2_pcs(axs[0], X_train_scaled, y_train, U, title="Train points of Iris with hyperplane")
+    plot_hyperplane_2pcs(axs[0], w_opt, b_opt, U)
+    plot_iris_2_class_2_pcs(axs[1], X_test_scaled, y_test, U, title="Test points of Iris")
+    plot_hyperplane_2pcs(axs[1], w_opt, b_opt, U)
+    fig.tight_layout()
+    plt.show()
